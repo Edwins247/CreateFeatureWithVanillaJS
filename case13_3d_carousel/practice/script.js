5edbff98ea61c768f48e25ba864da2e7b815f451
Edwins247 <cheewr85@daum.net>
2025-03-25 22:05:41 +0900

    case13 3d_carousel 구현(transform과 angle을 응용해서 구현)

--- case13_3d_carousel/practice/script.js
+++ case13_3d_carousel/practice/script.js
@@ -4,4 +4,26 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  const cellCount = 6
+  let selectIndex = 0
+  const prevButton = get('.prev_button')
+  const nextButton = get('.next_button')
+  const carousel = get('.carousel')
+
+  const rotateCarousel = () => {
+    // item에 정해진 css값과 반대로 angle을 줘야지 사진이 돌아가면서 보여짐(3d carousel이므로)
+    const angle = (selectIndex / cellCount) * -360
+    carousel.style.transform = `translateZ(-346px) rotateY(${angle}deg)`
+  }
+
+  prevButton.addEventListener('click', () => {
+    selectIndex--
+    rotateCarousel()
+  })
+  nextButton.addEventListener('click', () => {
+    selectIndex++
+    rotateCarousel()
+  })
+
 })()
