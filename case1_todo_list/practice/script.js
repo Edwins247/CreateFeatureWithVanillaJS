69e7d99aa6b748eeb32e155e2379cb706873a4cf
Edwins247 <cheewr85@daum.net>
2025-03-25 16:08:16 +0900

    API 연동하여 데이터 수정 및 삭제 기능 구현

--- case1_todo_list/practice/script.js
+++ case1_todo_list/practice/script.js
@@ -108,12 +108,77 @@
 
   }
 
+  const changeEditMode = (e) => {
+    // 수정을 위해 선택한 edit button중 가까운 item을 찾아 해당 요소들을 다 찾아옴
+    const $item = e.target.closest('.item');
+    const $label = $item.querySelector('label');
+    const $editInput = $item.querySelector('input[type="text"]');
+    const $contentButtons = $item.querySelector('.content_buttons');
+    const $editButtons = $item.querySelector('.edit_buttons');
+    const value = $editInput.value;
+
+    if (e.target.className === 'todo_edit_button') {
+      // edit button이 눌릴 때, input으로 입력하고 수정 확인, 취소 버튼이 나오게 처리함
+      $label.style.display = 'none'
+      $editInput.style.display = 'block'
+      $contentButtons.style.display = 'none'
+      $editButtons.style.display = 'block'
+      // edit 상태이므로 focus를 줌(커서를 끝으로 보냄)
+      $editInput.focus()
+      $editInput.value = ''
+      $editInput.value = value
+    }
+
+    if (e.target.className === 'todo_edit_cancel_button') {
+      // edit에서 취소를 하면 원래대로 돌아가게 처리
+      $label.style.display = 'block'
+      $editInput.style.display = 'none'
+      $contentButtons.style.display = 'block'
+      $editButtons.style.display = 'none'
+      // 만약 취소시, 기존에 쓰던 텍스트를 초기화 시킴
+      $editInput.value = $label.innerText
+    }
+  }
+
+  const editTodo = (e) => {
+    if (e.target.className !== 'todo_edit_confirm_button') return;
+    // confirm_button과 가까운 태그 찾아옴
+    const $item = e.target.closest('.item');
+    const id = $item.dataset.id;
+    const editInput = $item.querySelector('input[type="text"]');
+    const content = editInput.value;
+
+    fetch(`${API_URL}/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'        
+      },
+      body: JSON.stringify({ content }),
+    })
+      .then(getTodos)
+      .catch((error) => console.error(error));
+  }
+
+  const removeTodo = (e) => {
+    // remove button을 기준으로 가까운 item에 대해서 DELETE 처리를 진행
+    if (e.target.className !== 'todo_remove_button') return;
+    const $item = e.target.closest('.item');
+    const id = $item.dataset.id;
+
+    fetch(`${API_URL}/${id}`,{
+      method: 'DELETE',
+    }).then(getTodos).catch((error) => console.error(error));
+  }
+
   const init = () => {
     window.addEventListener('DOMContentLoaded', () => {
       getTodos();
     })
     $form.addEventListener('submit', addTodo);
     $todos.addEventListener('click', toggleTodo);
+    $todos.addEventListener('click', changeEditMode);
+    $todos.addEventListener('click', editTodo);
+    $todos.addEventListener('click', removeTodo);
   }
   init()
 })()
