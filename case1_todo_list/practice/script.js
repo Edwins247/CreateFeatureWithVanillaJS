db134745ed60bb33af690872b0618d6a6a8b685e
Edwins247 <cheewr85@daum.net>
2025-03-25 14:54:03 +0900

    API 연동하여 데이터 입력하여 POST 요청 기능 개발

--- case1_todo_list/practice/script.js
+++ case1_todo_list/practice/script.js
@@ -5,8 +5,12 @@
     return document.querySelector(target)
   }
 
+  const API_URL = 'http://localhost:3000/todos';
   // $을 붙인 이유는 단 하나의 변수로 구분하기 위함이고, 이를 El을 붙여서 구분할 수도 있음
   const $todos = get('.todos');
+  // Post 요청을 위해 form과 input el을 가져옴
+  const $form = get('.todo_form');
+  const $todoInput = get('.todo_input');
 
   const createTodoElement = (item) => {
     const { id, content } = item
@@ -52,16 +56,32 @@
   }
 
   const getTodos = () => {
-    fetch('http://localhost:3000/todos')
+    fetch(API_URL)
       .then((response) => response.json())
       .then((todos) => renderAllTodos(todos))
       .catch((error) => console.log(error));
+  }
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    const todo = {
+      content: $todoInput.value,
+      completed: false,
+    }
+    fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(todo),
+    })
   }
 
   const init = () => {
     window.addEventListener('DOMContentLoaded', () => {
       getTodos();
     })
+    $form.addEventListener('submit', addTodo);
   }
   init()
 })()
