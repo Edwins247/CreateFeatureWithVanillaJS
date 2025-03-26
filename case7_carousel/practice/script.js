9f525a3c73b396ad8a98ab422917ef4e474f6c83
Edwins247 <cheewr85@daum.net>
2025-03-26 15:39:03 +0900

    case7 이미지 슬라이더 구현(class를 인덱스 값을 바탕으로 읽어서 구분하여 자연스러운 캐러셀 구현)

--- case7_carousel/practice/script.js
+++ case7_carousel/practice/script.js
@@ -6,66 +6,38 @@
   }
 
   class Carousel {
-    constructor(caruoselElement) {
-      this.caruoselElement = caruoselElement
+    constructor(carouselElement) {
+      this.carouselElement = carouselElement
       this.itemClassName = 'carousel_item'
-      this.items = this.caruoselElement.querySelectorAll('.carousel_item')
+      this.items = this.carouselElement.querySelectorAll('.carousel_item')
+
       this.totalItems = this.items.length
       this.current = 0
+      // 버튼 눌렀을 때 이미지 슬라이더가 이벤트가 동작하지 않게 하기 위한 변수
+      this.isMoving = false
     }
 
     initCarousel() {
+      this.isMoving = false
       this.items[this.totalItems - 1].classList.add('prev')
       this.items[0].classList.add('active')
       this.items[1].classList.add('next')
     }
 
-    moveCarouselTo() {
-      let prev = this.current - 1
-      let next = this.current + 1
-
-      if (this.current === 0) {
-        prev = this.totalItems - 1
-      } else if (this.current === this.totalItems - 1) {
-        next = 0
-      }
-
-      for (let i = 0; i < this.totalItems; i++) {
-        if (i === this.current) {
-          this.items[i].className = this.itemClassName + ' active'
-        } else if (i === prev) {
-          this.items[i].className = this.itemClassName + ' prev'
-        } else if (i === next) {
-          this.items[i].className = this.itemClassName + ' next'
-        } else {
-          this.items[i].className = this.itemClassName
-        }
-      }
+    // 버튼을 누를 때 자연스럽게 넘기도록 무조건적으로 넘어가지 않게 제안을 걸어둠
+    disabledInteraction() {
+      this.isMoving = true
+      setTimeout(() => {
+        this.isMoving = false
+      }, 500)
     }
 
-    movePrev() {
-      if (this.current === 0) {
-        this.current = this.totalItems - 1
-      } else {
-        this.current--
-      }
-      this.moveCarouselTo()
-    }
-
-    moveNext() {
-      if (this.current === this.totalItems - 1) {
-        this.current = 0
-      } else {
-        this.current++
-      }
-      this.moveCarouselTo()
-    }
-
-    setEventListeners() {
-      this.prevButton = this.caruoselElement.querySelector(
+    // 현 carousel 아이템 기준으로 다음 & 이전 아이템에 대한 처리 필요함
+    setEventListener() {
+      this.prevButton = this.carouselElement.querySelector(
         '.carousel_button--prev'
       )
-      this.nextButton = this.caruoselElement.querySelector(
+      this.nextButton = this.carouselElement.querySelector(
         '.carousel_button--next'
       )
 
@@ -76,12 +48,65 @@
         this.moveNext()
       })
     }
+
+    moveCarouselTo() {
+      if (this.isMoving) return
+      this.disabledInteraction()
+      let prev = this.current - 1
+      let next = this.current + 1
+
+      // carousel이 움직임에 따라 숫자를 처리하는 로직
+      if (this.current === 0) {
+        prev = this.totalItems - 1
+      } else if (this.current === this.totalItems - 1) {
+        next = 0
+      }
+
+      // carousel에 속성에 맞게 className을 붙이기
+      for (let i = 0; i < this.totalItems; i++) {
+        if (i == this.current) {
+          this.items[i].className = this.itemClassName + ' active'
+        } else if (i == prev) {
+          this.items[i].className = this.itemClassName + ' prev'
+        } else if (i == next) {
+          this.items[i].className = this.itemClassName + ' next'
+        } else {
+          this.items[i].className = this.itemClassName 
+        }
+      }
+    }
+
+    moveNext() {
+      if (this.isMoving) return
+      // 처음으로 돌아가야 하므로 0이 됨
+      if (this.current === this.totalItems - 1) {
+        this.current = 0
+      } else {
+        // 아닌 경우 하나씩 늘림
+        this.current++
+      }
+      this.moveCarouselTo()
+    }
+
+    movePrev() {
+      if (this.isMoving) return
+      if (this.current === 0) {
+        this.current = this.totalItems - 1
+      } else {
+        this.current--
+      }
+      this.moveCarouselTo()
+    }
+  
   }
+
 
   document.addEventListener('DOMContentLoaded', () => {
     const carouselElement = get('.carousel')
     const carousel = new Carousel(carouselElement)
+
     carousel.initCarousel()
-    carousel.setEventListeners()
+    carousel.setEventListener()
   })
+
 })()
